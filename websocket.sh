5f136931f1c38788ff1b7c4546e63606f471e2cc
bottes-squad-team <75607956+YakkoRasZ14@users.noreply.github.com>
2021-09-19 03:27:51 +0700

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -9,7 +9,7 @@ echo Cek Hak Akses...
 sleep 0.5
 cd
 cd /usr/local/bin/
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ws-stunnel
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/YakkoRasZ14/gaspoll/main/ws-stunnel
 chmod +x /usr/local/bin/ws-stunnel
 
 # Installing Service
